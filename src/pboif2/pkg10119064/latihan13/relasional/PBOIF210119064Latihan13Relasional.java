719ebf4836222437dafd4576d62916e27d4c5138
Bagus_24 <Bagus_24@Bagus24>
2020-10-15 19:54:51 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan13/relasional/PBOIF210119064Latihan13Relasional.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan13.relasional;
+
+/**
+ *
+ * @author Bagus_24
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi program untuk melakukan operasi pembandingan
+ */
+public class PBOIF210119064Latihan13Relasional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 == bil1));
+    }
+    
+}
